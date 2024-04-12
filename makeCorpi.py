dbccfc4ae2a8d06453278502e579e8268b061e24
EvanMcCormick37 <87681272+EvanMcCormick37@users.noreply.github.com>
2024-04-12 19:14:24 -0400

    Cleaned Corpi and WDMs
    -Created cleaned corpi with lemmatized text
    -Created 2 WDMs, with lemmatization, one with all terms and 1 with top 100 by count
    -Removed the Dolphin reviews folder. I misread the assignment and thought i would have to review two different products. I just have to review 1 product.
    -Reorganized Reviews folder to make sense for 1 product. Now it's just a Positive and Negative folder.

---
+++ makeCorpi.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import re
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+df = pd.DataFrame(columns=['LABEL','REVIEW'])
+for i in range(1,16):
+    for sentiment in ['Positive','Negative']:
+            f = open(f'data/{sentiment}/{i}.txt','r')
+            content = f.read()
+            df.loc[len(df.index)]=[sentiment,content]
+
+
+wnl = WordNetLemmatizer()
+
+def cleanLemmatize(t):
+    text = re.sub("[^a-zA-Z'\\s+]","", t)
+    text = re.split("\\s+",text)
+    return ' '.join([wnl.lemmatize(word.lower()) for word in text])
+
+v_CL = np.vectorize(cleanLemmatize)
+df['REVIEW'] = v_CL(df['REVIEW'])
+
+CV1 = CountVectorizer(stop_words='english')
+CV2 = CountVectorizer(stop_words='english',max_features=100)
+
+wdm1 = pd.concat([ 
+    df['LABEL'],
+    pd.DataFrame(CV1.fit_transform(df['REVIEW']).toarray(),columns=CV1.get_feature_names_out())
+    ], axis=1)
+wdm2 = pd.concat([
+    df['LABEL'],
+    pd.DataFrame(CV2.fit_transform(df['REVIEW']).toarray(),columns=CV2.get_feature_names_out())
+    ], axis=1)
+
+wdm1.to_csv('data/wdms/WDMAll.csv')
+wdm2.to_csv('data/wdms/WDM100.csv')
--- maketxt.py
+++ maketxt.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 12 00:34:00 2024
-
-@author: Owner
-"""
-
+for i in range(1,16):
+    for item in ['bow','dolphin']:
+        for sentiment in ['pos','neg']:
+            f = open(f'data/reviews/{item}/{sentiment}/{i}.txt','w')
+            f.close()
